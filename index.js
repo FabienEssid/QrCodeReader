37a07428b5c3bb2e395d1bdc04397e26fe672101
Fabien Essid <fabien.essid@bearstudio.fr>
2021-08-02 21:49:44 +0200

    FEAT : Add react-native-magnus component library

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
+import { ThemeProvider } from 'react-native-magnus';
 import App from './App';
 import { name as appName } from './app.json';
 
 export default function Main() {
-  return <App />;
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
 }
 
 AppRegistry.registerComponent(appName, () => Main);
