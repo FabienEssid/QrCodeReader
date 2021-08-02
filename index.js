77233d61f21f16d37a5bd3217eafa23fc8083c43
Fabien Essid <fabien.essid@bearstudio.fr>
2021-08-02 21:17:06 +0200

    FEAT : Improve .prettierrc.js + lint existing files

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return <App />;
+}
+
+AppRegistry.registerComponent(appName, () => Main);
